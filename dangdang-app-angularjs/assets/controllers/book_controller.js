2da590a70cb916e74cdf94799c97e49f8f99b216
weiteng <a6872530a@163.com>
2016-10-19 17:04:34 +0800

    完善当当网界面

---
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -0,0 +1,8 @@
+app.controller('bookController', ['$scope', function($scope){
+    $scope.bookTypes = [
+        {id: 'ertong', name:'儿童', img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2077810900,2007243753&fm=116&gp=0.jpg', description: '那他们两个吧'},
+        {id: 'lishi', name:'历史', img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2077810900,2007243753&fm=116&gp=0.jpg', description: '那他们两个吧'},
+        {id: 'dongman', name:'动漫', img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2077810900,2007243753&fm=116&gp=0.jpg', description: '那他们两个吧'},
+        {id: 'qingchunwenxue', name:'青春文学', img: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2077810900,2007243753&fm=116&gp=0.jpg', description: '那他们两个吧'}
+    ]
+}])
--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,5 +1,10 @@
 app.controller('mainController', ['$scope', 'commonService', function($scope, c_service){
-    c_service.getData('dongman', function(res){
-        console.dir(res);
-    })
+    // c_service.getData('dongman', function(res){
+    //     console.dir(res);
+    // })
+
+    $scope.selectIndex = 0;
+    $scope.linkTo = function(index){
+        $scope.selectIndex = index;
+    }
 }])
