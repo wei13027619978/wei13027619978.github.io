5405bff573a82d8baac2dba73b590dc98c711c73
weiteng <a6872530a@163.com>
2016-10-19 16:00:29 +0800

    修改路径

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -2,7 +2,7 @@ app.factory('commonService', ['$http', function($http){
     var service = {};
     service.getData = function(type, callBack){
         $http({
-            url:'/data/book_'+ type +'.json',
+            url:'/dangdang-app-angularjs/data/book_'+ type +'.json',
             method:'get'
         }).then(function(res){
             console.log('获取数据成功');
