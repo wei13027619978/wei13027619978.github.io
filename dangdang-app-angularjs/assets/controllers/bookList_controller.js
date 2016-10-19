5d8c4771ef48ae585829220cedf9b2adbbfcb8e0
weiteng <a6872530a@163.com>
2016-10-19 21:30:28 +0800

    77777

--- dangdang-app-angularjs/assets/controllers/bookList_controller.js
+++ dangdang-app-angularjs/assets/controllers/bookList_controller.js
@@ -1,6 +1,7 @@
 app.controller('bookListController',  
 ['$scope', '$routeParams', 'commonService', 
 function($scope, $routeParams, c_service){
+    console.log($routeParams.id);
     c_service.getData($routeParams.id, function(res){
         console.dir(res.data);
     })
